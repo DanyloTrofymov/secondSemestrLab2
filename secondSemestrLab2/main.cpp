c0876eb323a7ae8d0e1e0bdfba22e4ffbf0b25e9
Olha Shevel <o.v.shevel@gmail.com>
2021-02-21 22:01:48 +0200

    function discardContract() started

--- secondSemestrLab2/main.cpp
+++ secondSemestrLab2/main.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include<vector>
 using namespace std;
 
 string getDirectory();
 void createFiles(string dir);
-//void discardContract();
+void discardContract();
+
 //void averageScore();
 //void sort();
 //void ratingOut();
 
+
 void main() {
-    string dir = getDirectory();
-    createFiles(dir);
+ /*   string dir = getDirectory();
+    createFiles(dir);*/
+    discardContract();
 }
+
+
 
 string getDirectory() {
     string dir;
@@ -25,4 +31,26 @@ string getDirectory() {
 void createFiles(string dir) {
     ofstream fout(dir + "rating.csv");
     ofstream temp(dir + "temp.csv");
+}
+
+void discardContract() {
+    string number;
+    string line;
+  
+    ifstream in("C:\\Users\OlhaShevel\Desktop\New folder\students1.csv");
+   
+        if (in.is_open()) {
+            getline(in, number);
+            int num = stoi(number);
+            cout << num;
+            for (int i = 1; i = num; i++) {
+                getline(in, line);
+                int pos = line.find_last_not_of(',');
+                
+                cout<< line.substr(pos, line.length());
+            }
+
+        
+        }
+       // in.close();
 }

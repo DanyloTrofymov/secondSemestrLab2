854840cc2a6f19a32b0c58b6321180223c11112b
Danil <danya.trofimov@gmail.com>
2021-02-23 11:19:04 +0200

    function for finding CSV`s

--- secondSemestrLab2/main.cpp
+++ secondSemestrLab2/main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>;
+#include <direct.h>
+#include <io.h>
 using namespace std;
 
 string getDirectory();
 void createFiles(string dir);
+vector<string> findCsv(string dir);
 //void discardContract();
 //void averageScore();
 //void sort();
@@ -13,6 +17,7 @@ void createFiles(string dir);
 void main() {
     string dir = getDirectory();
     createFiles(dir);
+    vector<string> csvFiles = findCsv(dir);
 }
 
 string getDirectory() {
@@ -25,4 +30,31 @@ string getDirectory() {
 void createFiles(string dir) {
     ofstream fout(dir + "rating.csv");
     ofstream temp(dir + "temp.csv");
+}
+
+vector<string> findCsv(string dir) {
+    vector<string> allFiles;
+    string csv = "\\*.csv";
+    char charDir[200];
+
+    for (int i = 0; i < dir.length(); i++)
+    {
+        charDir[i] = dir[i];
+    }
+
+    int j = 0;
+    for (int i = dir.length(); i < dir.length()+7; i++)
+    {
+        charDir[i] = csv[j];
+        j++;
+    }
+
+    _finddata_t data;
+    intptr_t handle = _findfirst(charDir, &data);
+    do {
+        allFiles.push_back(data.name);
+    } while (_findnext(handle, &data) == 0);
+    _findclose(handle);
+
+    return allFiles;
 }

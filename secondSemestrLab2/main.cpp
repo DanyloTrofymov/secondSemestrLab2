f31f28c5cdb1867c73d90d2f21ef3f095915f8df
Danil <danya.trofimov@gmail.com>
2021-02-23 13:50:26 +0200

    comments, minor changes

--- secondSemestrLab2/main.cpp
+++ secondSemestrLab2/main.cpp
@@ -1,34 +1,26 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include<vector>
-#include <vector>;
+#include <vector>
 #include <direct.h>
 #include <io.h>
 using namespace std;
+string getDirectory();      //gets directory 
+//void createFiles(string dir);
+void discardContract(string dir, vector <string> allFiles); // writes all students with attribute FALSE in csv
 
-string getDirectory();
-void createFiles(string dir);
-void discardContract(string dir, vector <string> allFiles);
-
-vector<string> findCsv(string dir);
-//void discardContract();
-//void averageScore();
-//void sort();
+vector<string> findCsv(string dir); // Finds all .csv files in directiry 
+//void averageScore(); // counts the avarage score of every student
+//void sort(); // sotrs list of students by avarage score
 //void ratingOut();
 
 
 void main() {
+    setlocale(LC_ALL, " ");
     string dir = getDirectory();
-
     vector<string> csvFiles = findCsv(dir);
- //   createFiles(dir);
-
-  
     discardContract(dir, csvFiles);
 }
-
-
 
 string getDirectory() {
     string dir;
@@ -46,57 +38,49 @@ void discardContract(string dir, vector<string> allFiles) {
     string number;
     string line;
     ofstream out(dir + "\\temp.csv");
- 
+    
     for (int i = 0; i < allFiles.size(); i++)
     {
-
-
         ifstream in(dir + "\\" + allFiles[i]);
-     //   ofstream out(dir + "\\temp.csv");
-
-
         if (in.is_open()) {
             getline(in, number);
-            cout << number;
             int num = stoi(number);
-            cout << num;
-            
             for (int j = 1; j < num; j++) {
                 getline(in, line);
                 int pos = line.find_last_of(',') + 1;
 
                 string status = line.substr(pos, line.length());
-                cout << status;
                 if (status == "FALSE") {
-                    out << line.substr(0, pos) << endl;
+                    out << line.substr(0, pos-1) << endl;
                 }
             }
-
-
-
+            
         }
+        in.close();
     }
+    out.close();
 }
-       // in.close();
+
 
 
 vector<string> findCsv(string dir) {
     vector<string> allFiles;
-    string csv = "\\*.csv";
-    char charDir[200];
+    string csv = "\\*.csv";     // file type
+    char charDir[200];  // makes const char for using on searching files function 
 
-    for (int i = 0; i < dir.length(); i++)
+    for (int i = 0; i < dir.length(); i++) // writes directory in const char
     {
         charDir[i] = dir[i];
     }
 
-    int j = 0;
-    for (int i = dir.length(); i < dir.length()+7; i++)
+    int j = 0; 
+    for (int i = dir.length(); i < dir.length()+ csv.length()+1; i++)   // writes file type in const char
     {
         charDir[i] = csv[j];
         j++;
     }
 
+    //searching .csv files function
     _finddata_t data;
     intptr_t handle = _findfirst(charDir, &data);
     do {
